b8b12bf79c44488334f3c2fa6bb0eaf611b42c3d
TheShubham-K <subham.kumar03@gmail.com>
2020-06-29 09:54:00 +0530

    assignment 4

--- result/assign4.py
+++ result/assign4.py
@@ -1,34 +1,26 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture('./result/assign4_1fps.avi')
-red  = np.zeros((500,600,3))
-blue  = np.zeros((500,600,3))
-green  = np.zeros((500,600,3))
-
-red = cv2.rectangle(red, (50,50),(350,150), (0,0,255),5)
-green = cv2.rectangle(green, (50,50),(350,150), (0,255,0),5)
-blue = cv2.rectangle(blue, (50,50),(350,150), (255,0,0),5)
-
 fcc = cv2.VideoWriter_fourcc("X", "V", "I", "D")
-out = cv2.VideoWriter('./result/assign4_1fps.avi', fcc, 5.0, (400, 200))
+out = cv2.VideoWriter('./result/assignment_1fps.avi', fcc, 1.0, (400, 200))
 
-while True:
-    count = 40
-    
-    while count>0:
-        ret, frame = cap.read()
-        cv2.imshow("Frame", frame)
-        out.write(red)
-        ret, frame = cap.read()
-        cv2.imshow("Frame", blue)
-        out.write(blue)
-        ret, frame = cap.read()
-        cv2.imshow("Frame", green)
-        out.write(green)
-        count -= 1
-    
-    if cv2.waitKey(1) == 27:
-        break
+red  = np.zeros((200,400,3))
+red[75:125,100:300] = [0,0,255]
+blue  = np.zeros((200,400,3))
+blue[75:125,100:300] = [255,0,0]
+green  = np.zeros((200,400,3))
+green[75:125,100:300] = [0,255,0]
 
+
+cv2.imwrite('result/red.jpg',red)
+cv2.imwrite('result/blue.jpg',blue)
+cv2.imwrite('result/green.jpg',green)
+
+for i in range(1,41):
+    red = cv2.imread('result/red.jpg')
+    blue = cv2.imread('result/blue.jpg')
+    green = cv2.imread('result/green.jpg')
+    out.write(red)
+    out.write(blue)
+    out.write(green)
 out.release()
