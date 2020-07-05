a74c4582f4d93f476671a928392ce9e4a5d86d10
TheShubham-K <subham.kumar03@gmail.com>
2020-07-05 08:50:44 +0530

    class 3 py file

--- result/class03.py
+++ result/class03.py
@@ -1,25 +1,24 @@
 import cv2
 import matplotlib.pyplot as plt
-import numpy as np
 
-img = np.zeros((500, 650, 3))
+img = cv2.imread('res/lena.jpg', 1)
+px = img[50,50]
+print(px)
+# img[100,100] = [0,0,0]
+# img[100,300] = [255,255,255]
 
+# img[50:150, 150:200] = [0,0,0]
+# img[50:150, 200:250] = [255,255,255]
 
-img = cv2.line(img, (520, 250), (150, 250), (255, 115, 5), 5)
-img = cv2.rectangle(img, (30, 220), (120, 270), (255, 0, 5), -1)
-img = cv2.circle(img, (590, 250), 55, (0, 255, 0), -1)
-
-font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
-string = "Shubham Kumar"
-img = cv2.putText(img, string, (175, 450), font, 1.5, (255, 0, 0), 1)
-pts = np.array([[0, 0], [650, 495], [0, 495], [650, 0]], np.int32)
-img = cv2.polylines(img, [pts], True, (0, 0, 255), 2)
-
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+roi = img[200:400, 200:400]
+img[0:200, 0:200] = roi
 
 
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-# plt.imshow(img)
-# plt.show()
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(img)
+plt.show()
+
+
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
