b6e8b872a086778b14b4e6e52a580b861c9c1617
TheShubham-K <subham.kumar03@gmail.com>
2020-07-05 08:50:53 +0530

    class 4 py file

--- result/class04.py
+++ result/class04.py
@@ -1,24 +1,36 @@
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread('res/lena.jpg', 1)
-px = img[50,50]
-print(px)
-# img[100,100] = [0,0,0]
-# img[100,300] = [255,255,255]
+img1 = cv2.imread("res/logic_1.jpg")
+img2 = cv2.imread("res/logic_2.jpg")
 
-# img[50:150, 150:200] = [0,0,0]
-# img[50:150, 200:250] = [255,255,255]
-
-roi = img[200:400, 200:400]
-img[0:200, 0:200] = roi
+bit_and = cv2.bitwise_and(img1, img2)
+bit_or = cv2.bitwise_or(img1, img2)
+bit_xor = cv2.bitwise_xor(img1, img2)
+img1_not = cv2.bitwise_not(img1)
+img2_not = cv2.bitwise_not(img2)
 
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.imshow(img)
-plt.show()
+cv2.imshow("AND", bit_and)
+cv2.imwrite("result/AND.jpg", bit_and)
+cv2.imshow("OR", bit_or)
+cv2.imwrite("result/OR.jpg", bit_or)
+cv2.imshow("XOR", bit_xor)
+cv2.imwrite("result/XOR.jpg", bit_xor)
+cv2.imshow("Img 01 NOT", img1_not)
+cv2.imwrite("result/Img01NOT.jpg", img1_not)
+cv2.imshow("Img 02 NOT", img2_not)
+cv2.imwrite("result/Img02NOT.jpg", img2_not)
 
+# titles = ['img1','img2','bit_and','bit_or', 'bit_xor','img1_not','img2_not']
+# images =[img1,img2,bit_and,bit_or,bit_xor,img1_not,img2_not]
 
-# cv2.imshow('image', img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# for i in range(len(titles)):
+#     plt.subplot(2,4,i+1),plt.imshow(images[i],'gray')
+#     plt.title(titles[i])
+#     plt.xticks([]),plt.yticks([])
+# plt.show()
+    
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
